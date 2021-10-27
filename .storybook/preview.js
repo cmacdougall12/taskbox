5cb5e892afea45e8e1c8408546dbc4751e80ec36
cmacdougall12 <cammacdougall12@gmail.com>
2021-10-27 13:12:16 -0600

    simple component testing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,17 @@
+import '../src/index.css';
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// };
+
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 };
